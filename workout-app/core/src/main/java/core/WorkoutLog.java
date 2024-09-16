f862b5ff87f00a1b5daa3e035e93e30026de53fa
Eline Fondevik <elinefondevik@Eline-sin-Brbar-datamaskin.local>
2024-09-16 21:07:32 +0200

    Added unstaged changes

--- workout-app/core/src/main/java/core/WorkoutLog.java
+++ workout-app/core/src/main/java/core/WorkoutLog.java
@@ -1,5 +1,28 @@
 package core;
 
-public class WorkoutLog {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class WorkoutLog implements Iterable<Workout> { //log that iterates over workouts
     
+    private List<Workout> workouts; //collects workout objects in a list
+
+    public WorkoutLog() { //constructor initiates the list of workouts
+        this.workouts = new ArrayList<>();
+    }
+
+    public void addWorkout(Workout workout) { //adds a workout to the log
+        workouts.add(workout);
+    }
+
+    public List<Workout> getWorkouts() {
+        return workouts;
+    }
+
+    @Override
+    public Iterator<Workout> iterator() { //iterator
+        return workouts.iterator();
+    }
+
 }
