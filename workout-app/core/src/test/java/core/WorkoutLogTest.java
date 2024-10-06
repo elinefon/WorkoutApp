e8a302b59eb11249e1c9680f82dd0ebe5ddf99a8
Ingvild Kirkaune Sandven <ingviksa@stud.ntnu.no>
2024-10-06 21:34:43 +0200

    [core] Changed workoutlog to return copy of workouts and removed excessive imports in both core and persistence (#30)

--- workout-app/core/src/test/java/core/WorkoutLogTest.java
+++ workout-app/core/src/test/java/core/WorkoutLogTest.java
@@ -1,8 +1,6 @@
 package core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WorkoutLogTest {
